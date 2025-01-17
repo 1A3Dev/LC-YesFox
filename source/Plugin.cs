f913048adec50abb73f633821539a13c43347f39
ButteryStancakes <jlj3322@gmail.com>
2025-01-16 19:52:24 -0500

    Fix a couple broken references (oops)

--- source/Plugin.cs
+++ source/Plugin.cs
@@ -461,10 +461,10 @@ namespace YesFox
                 {
                     if (__instance.IsServer)
                     {
-                        if (!ES3.Load($"YesFox_{StartOfRound.Instance.currentLevel.name}_Success", __instance.currentSaveFileName, false))
+                        if (!ES3.Load($"YesFox_{StartOfRound.Instance.currentLevel.name}_Success", GameNetworkManager.Instance.currentSaveFileName, false))
                         {
                             // weed growth has succeeded at least one time for this planet
-                            ES3.Save($"YesFox_{StartOfRound.Instance.currentLevel.name}_Success", true, __instance.currentSaveFileName);
+                            ES3.Save($"YesFox_{StartOfRound.Instance.currentLevel.name}_Success", true, GameNetworkManager.Instance.currentSaveFileName);
                             Plugin.logSource.LogDebug($"Mold growth on \"{StartOfRound.Instance.currentLevel.PlanetName}\" succeeded for first time");
                         }
                     }
@@ -475,7 +475,7 @@ namespace YesFox
                     if (__instance.IsServer)
                     {
                         // restore the previous values so weeds won't reset their growth cycle or change origin
-                        if (ES3.Load($"YesFox_{StartOfRound.Instance.currentLevel.name}_Success", __instance.currentSaveFileName, false))
+                        if (ES3.Load($"YesFox_{StartOfRound.Instance.currentLevel.name}_Success", GameNetworkManager.Instance.currentSaveFileName, false))
                         {
                             StartOfRound.Instance.currentLevel.moldSpreadIterations = iterations;
                             StartOfRound.Instance.currentLevel.moldStartPosition = __state;
@@ -499,7 +499,7 @@ namespace YesFox
 
             foreach (SelectableLevel level in __instance.levels)
             {
-                ES3.DeleteKey($"YesFox_{level.name}_Success", __instance.currentSaveFileName);
+                ES3.DeleteKey($"YesFox_{level.name}_Success", GameNetworkManager.Instance.currentSaveFileName);
                 Plugin.logSource.LogDebug($"Reset mold growth success for \"{level.PlanetName}\" (ship reset)");
             }
         }
@@ -512,7 +512,7 @@ namespace YesFox
 
             if (StartOfRound.Instance.currentLevel.moldSpreadIterations < 1)
             {
-                ES3.DeleteKey($"YesFox_{StartOfRound.Instance.currentLevel.name}_Success", __instance.currentSaveFileName);
+                ES3.DeleteKey($"YesFox_{StartOfRound.Instance.currentLevel.name}_Success", GameNetworkManager.Instance.currentSaveFileName);
                 Plugin.logSource.LogDebug($"Reset mold growth success for \"{StartOfRound.Instance.currentLevel.PlanetName}\" (all spores destroyed)");
             }
         }
